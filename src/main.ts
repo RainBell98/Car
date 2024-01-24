0717fac5e81634e83e4628f98c553ea9ce0bce94
RainBell98 <ttiger45@naver.com>
2024-01-24 18:07:15 +0900

    [Study] nest-test(3)

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
+import { setupApp } from "./set-up";
 
 const cookieSession = require('cookie-session')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys:['asdfasdf']
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    })
-  )
+  setupApp(app)
   await app.listen(3000);
 }
 bootstrap();
