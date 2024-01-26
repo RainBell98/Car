8d26d30789f69ed7ceafc62c873c5aca2bb31a3b
RainBell98 <ttiger45@naver.com>
2024-01-26 13:45:27 +0900

    [Study] CreateReports

--- configs/typeorm.config.ts
+++ configs/typeorm.config.ts
@@ -2,15 +2,17 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 import { User } from "../src/users/user.entity";
 import { Report } from "../src/reports/report.entity";
-
+import * as dotenv from 'dotenv';
+import * as process from "process";
+dotenv.config()
 export const typeOrmConfig : TypeOrmModuleOptions ={
 
   "type": "postgres",
   "host": "localhost",
   "port": 5432,
   "username": "mycv",
-  "password": "dldnwhd@",
-  "database": "mycvDB",
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
   "entities": ["dist/**/**.entity{.ts,.js}",User],
   "synchronize": true
 
